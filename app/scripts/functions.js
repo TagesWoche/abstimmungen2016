a5d000ba544850163745db7559677d0a2a446482
Felix Michel <felix_michel@gmx.ch>
2016-09-07 17:16:06 +0200

    changes to chart

--- app/scripts/functions.js
+++ app/scripts/functions.js
@@ -14,12 +14,24 @@ $(function () {
     },
 
     lang: {
-        drillUpText: '◁ Zurück zu {series.name}'
+        drillUpText: '◁ Zurück zur Übersicht'
     },
 
     chart: {
       type: 'bar',
       spacingTop: 40,
+      events: {
+        drilldown: function (e) {
+          this.setTitle({text: e.point.name});
+          this.yAxis[0].axisTitle.attr({
+            text: 'Stimmenanteile der Regierungskandidaten nach Partei' });
+        },
+        drillup: function(e) {
+          this.setTitle({ text: '' });
+          this.yAxis[0].axisTitle.attr({
+            text: 'Stimmenanteil am Total aller gültigen Wahlzettel' });
+      }
+      }
     },
     title: {
       text: null
@@ -60,7 +72,7 @@ $(function () {
 
     series: [{
       name: 'Kandidaten',
-      color: '#009655',
+      color: '#008148',
       data: [{
         name: 'Eva Herzog (SP)',
         y: 63,
@@ -82,26 +94,32 @@ $(function () {
         y: 51,
         drilldown: 'duerr'
       }, {
+        color: '#0f91a2',
         name: 'Hans-Peter Wessels (SP)',
         y: 48,
         drilldown: 'wessels'
       }, {
+        color: '#0f91a2',
         name: 'Lorenz Nägelin (SVP)',
         y: 40,
         drilldown: 'naegelin'
       }, {
+        color: '#0f91a2',
         name: 'Elisabeth Ackermann (Grüne)',
         y: 40,
         drilldown: 'ackermann'
       }, {
+        color: '#0f91a2',
         name: 'Heidi Mück (Basta!)',
         y: 32,
         drilldown: 'mueck'
       }, {
+        color: '#0f91a2',
         name: 'Martina Bernasconi (glp)',
         y: 21,
         drilldown: 'bernasconi'
       }, {
+        color: '#0f91a2',
         name: 'Andere',
         y: 17
       }
@@ -173,17 +191,94 @@ $(function () {
         id: 'engelberger',
         data: [
           ['CVP', 100],
-          ['BGB', 84],
-          ['EVP', 70],
-          ['glp', 63],
-          ['CVP', 40],
-          ['BDP', 39],
-          ['LDP', 35],
-          ['FDP', 20],
-          ['SVP', 11]
+          ['FDP', 96],
+          ['LDP', 92],
+          ['SVP', 73],
+          ['BDP', 68],
+          ['glp', 64],
+          ['EVP', 45],
+          ['SP', 25],
+          ['BGB', 12]
+        ]
+      }, {
+        id: 'duerr',
+        data: [
+          ['FDP', 94],
+          ['CVP', 84],
+          ['LDP', 83],
+          ['SVP', 76],
+          ['EVP', 58],
+          ['BDP', 56],
+          ['glp', 53],
+          ['SP', 19],
+          ['BGB', 8]
+        ]
+      }, {
+        id: 'wessels',
+        data: [
+          ['SP', 95],
+          ['BGB', 80],
+          ['glp', 58],
+          ['EVP', 50],
+          ['BDP', 35],
+          ['CVP', 13],
+          ['LDP', 9],
+          ['FDP', 7],
+          ['SVP', 5]
+        ]
+      }, {
+        id: 'naegelin',
+        data: [
+          ['SVP', 92],
+          ['FDP', 78],
+          ['LDP', 75],
+          ['BDP', 63],
+          ['CVP', 54],
+          ['glp', 32],
+          ['EVP', 31],
+          ['SP', 1],
+          ['BGB', 0]
+        ]
+      }, {
+        id: 'ackermann',
+        data: [
+          ['BGB', 89],
+          ['SP', 78],
+          ['EVP', 59],
+          ['glp', 39],
+          ['BDP', 16],
+          ['CVP', 7],
+          ['LDP', 4],
+          ['FDP', 1],
+          ['SVP', 0]
+        ]
+      }, {
+        id: 'mueck',
+        data: [
+          ['BGB', 75],
+          ['SP', 64],
+          ['EVP', 30],
+          ['glp', 13],
+          ['LDP', 6],
+          ['CVP', 2],
+          ['FDP', 1],
+          ['SVP', 1],
+          ['BDP', 0]
+        ]
+      }, {
+        id: 'bernasconi',
+        data: [
+          ['glp', 93],
+          ['EVP', 30],
+          ['SP', 27],
+          ['LDP', 19],
+          ['BGB', 18],
+          ['CVP', 12],
+          ['BDP', 11],
+          ['SVP', 8],
+          ['FDP', 8]
         ]
       }
-
 
       ]
     }
