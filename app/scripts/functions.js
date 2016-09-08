2d0695e33de7756c44e8dcaa53606294729144f9
Felix Michel <felix_michel@gmx.ch>
2016-09-08 14:17:50 +0200

    add sorgenbarometer charts

--- app/scripts/functions.js
+++ app/scripts/functions.js
@@ -98,32 +98,32 @@ $(function () {
         y: 51,
         drilldown: 'duerr'
       }, {
-        color: '#0f91a2',
+        color: '#00b766',
         name: 'Hans-Peter Wessels (SP)',
         y: 48,
         drilldown: 'wessels'
       }, {
-        color: '#0f91a2',
+        color: '#00b766',
         name: 'Lorenz Nägelin (SVP)',
         y: 40,
         drilldown: 'naegelin'
       }, {
-        color: '#0f91a2',
+        color: '#00b766',
         name: 'Elisabeth Ackermann (Grüne)',
         y: 40,
         drilldown: 'ackermann'
       }, {
-        color: '#0f91a2',
+        color: '#00b766',
         name: 'Heidi Mück (Basta!)',
         y: 32,
         drilldown: 'mueck'
       }, {
-        color: '#0f91a2',
+        color: '#00b766',
         name: 'Martina Bernasconi (glp)',
         y: 21,
         drilldown: 'bernasconi'
       }, {
-        color: '#0f91a2',
+        color: '#00b766',
         name: 'Andere',
         y: 17
       }
@@ -141,14 +141,9 @@ $(function () {
           'stroke-width': 1,
           stroke: 'black',
           r: 3,
-            title: {
-                  style: {
-           color: '#000',
-                  }
-            },
           states: {
             hover: {
-              fill: '#efefef',
+              fill: '#efefef'
             },
             select: {
               stroke: '#efefef',
@@ -358,12 +353,15 @@ $(function () {
       enabled: false
     },
     series: [{
+      color: '#008148',
       name: 'Umfrage 2016',
       data: [30.25, 16.85, 12.81, 11.53, 10.34, 6.9, 3.84, 1.18, 3.94, 0.59, 0.49, 1.28]
     }, {
+      color: '#00b766',
       name: 'Wahlen 2012',
       data: [30.7, 15, 9.6, 11.1, 11.8, 7.3, 5, 1.1, 4.2, 1.2, 0.4, 2.5]
     }, {
+      color: '#00e07d',
       name: 'Wahlen 2008',
       data: [29.7, 13.7, 8.4, 9.9, 13.8, 9.3, 5.1, 0, 4.4, 0, 2.4, 3.3]
     }]
@@ -435,6 +433,186 @@ $(function () {
       }
       ]
     }]
+  });
+});
+
+// sorgenbarometer 1, zusammenleben
+
+$(function () {
+  $('#sorgen1').highcharts({
+    exporting: {
+      enabled: false
+    },
+    tooltip: {
+      enabled: false
+    },
+    chart: {
+      type: 'bar'
+    },
+    title: {
+      text: null
+    },
+    xAxis: {
+      categories: ['«Es hat zu viele Ausländer und Ausländerinnen in der Stadt.»', '«Die Menschen in der Stadt begegnen einander zunehmend mit sozialer Kälte.»', '«Der Kanton Baselland profitiert übermässig von der Stadt Basel.»', '«Die kantonalen Krankenkassenprämien sind zu hoch.»', '«Die Steuern im Kanton sind zu hoch.»', '«Die Arbeitsplatzsicherheit bereitet mir Sorgen.»', '«Der Bund kümmert sich zu wenig um Basler Probleme.»']
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: null
+      },
+      labels: {
+        formatter: function () {
+        return this.value + "%";
+        }
+      }
+    },
+    legend: {
+      reversed: true,
+      align: 'right'
+    },
+    plotOptions: {
+      series: {
+        stacking: 'percent',
+          point: {
+            events: {
+              mouseOver: function (e) {
+                this.dataLabel.css({
+                  opacity: "1"
+                });
+              },
+              mouseOut: function (e) {
+                this.dataLabel.css({
+                  opacity: "0"
+                });
+              }
+            }
+          },
+        dataLabels: {
+          enabled: true,
+          format: '{point.y:.1f}%',
+          style: {
+            opacity: "0"
+          }
+        }
+      }
+    },
+    credits: {
+      enabled: false
+    },
+    series: [
+      {
+        color: '#00e07d',
+        name: 'sehr einverstanden',
+        data: [22.6, 17.6, 41.7, 68.5, 37.7, 19.9, 37.5]
+      }, {
+        color: '#00b766',
+        name: '4',
+        data: [11.8, 29.3, 28.6, 21.4, 22.6, 22.1, 28.3]
+      }, {
+        color: '#008148',
+        name: '3',
+        data: [17.6, 28.7, 17.7, 6.6, 21.8, 27.4, 21.9]
+      }, {
+      color: '#005831',
+      name: '2',
+      data: [16.7, 15.5, 7.8, 1.7, 10.7, 16.8, 9.1]
+    }, {
+        color: '#00371f',
+        name: 'gar nicht einverstanden',
+        data: [31.3, 8.8, 4.2, 1.9, 7.2, 13.8, 3.3]
+      }
+    ]
+  });
+});
+
+
+// sorgenbarometer 2, das Leben in der Stadt
+
+$(function () {
+  $('#sorgen2').highcharts({
+    exporting: {
+      enabled: false
+    },
+    tooltip: {
+      enabled: false
+    },
+    chart: {
+      type: 'bar'
+    },
+    title: {
+      text: null
+    },
+    xAxis: {
+      categories: ['«Das Lädeli-Sterben in der Stadt ist ein Problem.»', '«Es hat zu viel motorisierten Verkehr auf den Strassen.»', '«In der Stadt ist es zu wenig sauber.»', '«Es gibt zu wenige Grünflächen zum Erholen in der Stadt.»', '«Im öffentlichen Raum fühle ich mich unsicher.»', '«In der Stadt hat es zu viel Lärm.»'],
+      opposite: true
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: null
+      },
+      reversed: true,
+      labels: {
+        formatter: function () {
+          return this.value + "%";
+        }
+      }
+    },
+    legend: {
+      align: 'left'
+    },
+    plotOptions: {
+      series: {
+        stacking: 'percent',
+        point: {
+          events: {
+            mouseOver: function (e) {
+              this.dataLabel.css({
+                opacity: "1"
+              });
+            },
+            mouseOut: function (e) {
+              this.dataLabel.css({
+                opacity: "0"
+              });
+            }
+          }
+        },
+        dataLabels: {
+          enabled: true,
+          format: '{point.y:.1f}%',
+          style: {
+            opacity: "0"
+          }
+        }
+      }
+    },
+    credits: {
+      enabled: false
+    },
+    series: [
+      {
+        color: '#00e07d',
+        name: 'sehr einverstanden',
+        data: [36.1, 13.7, 16.3, 13.4, 9.1, 16.7]
+      }, {
+        color: '#00b766',
+        name: '4',
+        data: [29.9, 18.1, 18.2, 16.6, 17, 13.3]
+      }, {
+        color: '#008148',
+        name: '3',
+        data: [18.2, 25.5, 20.5, 27.7, 26, 14.7]
+      }, {
+        color: '#005831',
+        name: '2',
+        data: [8.6, 22.2, 18.9, 22.8, 26.7, 25.8]
+      }, {
+        color: '#00371f',
+        name: 'gar nicht einverstanden',
+        data: [7.2, 20.6, 26, 19.4, 21.2, 29.5]
+      }
+    ]
   });
 });
 
